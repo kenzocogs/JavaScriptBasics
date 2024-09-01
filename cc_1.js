745c018ecac803a3119e15185e6032d0e908d230
kenzocogs <kcogswell@usf.edu>
2024-09-01 18:50:02 -0400

    Task 1 - Employee Infomration

--- cc_1.js
+++ cc_1.js
@@ -1,2 +1,9 @@
+let employeeName = "Kenzo";
 
+const employeeID = 4743770;
+
+var isActive = true;
+
+console.log(employeeName, employeeID, isActive)
+console.log(typeof "Kenzo", typeof 4743770, typeof isActive)
 

20edfeebec32f1fc24d827d1cdaba906c0a6700a
kenzocogs <kcogswell@usf.edu>
2024-09-01 19:18:29 -0400

    Task 2 - Product Inventory Management

--- cc_1.js
+++ cc_1.js
@@ -5,5 +5,13 @@ const employeeID = 4743770;
 var isActive = true;
 
 console.log(employeeName, employeeID, isActive)
-console.log(typeof "Kenzo", typeof 4743770, typeof isActive)
+console.log(typeof "Kenzo", typeof 4743770, typeof isActive);
+
+let products = ["EasyWrite Pens", "EasyStick Tape", "EasyHold Envelopes"]
+const productDetails = {
+    "name": "EasyWrite Pens",
+    "price":  7.32,
+    "inStock": true,
+}
+console.log(products, productDetails)
 
